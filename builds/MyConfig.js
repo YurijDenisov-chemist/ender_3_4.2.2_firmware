de6d50d902588dd08ab770f515fce5cb82863583
Yurij <129675279+YurijDenisov-chemist@users.noreply.github.com>
2026-01-07 08:41:50 +0300

    Update MyConfig.js

--- builds/MyConfig.js
+++ builds/MyConfig.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     configuration: {
         enable: [
-        ["HEATER_0_MAXTEMP", 350],
+        ["HEATER_0_MAXTEMP", 330],
         ["BED_MAXTEMP", 150]
         ]}
 
